3a60ae5d4960b67cd11b8f7d26f50a3e71c3a5db
jorcolito <jorgecolamarco03@gmail.com>
2026-06-23 09:46:30 -0500

    Creacion de css, crear apptsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,26 @@
 import "./App.css";
 import { products } from "./data/products";
 import ProductCard from "./components/ProductCard";
+
+export default function App() {
+    const firstProduct = products[0];
+
+    return (
+        <main className="app">
+            <section className="hero">
+                <h1>Comernova</h1>
+                <p>Organización elegante para espacios sofisticados.</p>
+            </section>
+
+            <section className="products-section">
+                <h2>Productos destacados</h2>
+
+                <div className="products-section">
+                    {products.map((product)=> (
+                        <ProductCard key={product.id} product={product} />
+                    ))}
+                </div>
+            </section>
+        </main>
+    );
+}
